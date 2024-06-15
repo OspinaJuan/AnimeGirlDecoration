455fdf0407015be0cd5ffdcc1eed93a6780c39f8
OspinaJuan <ospinaangaritaoa@gmail.com>
2024-06-15 16:08:21 -0500

    Finished all movement that runs from the mouse pointer. However it doesn't speed when the mouse is getting closer from behind.

--- animegirlv2.py
+++ animegirlv2.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QLabel, QDesktopWidget
 from PyQt5.QtCore import Qt, QTimer, QUrl
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QCursor
 from PyQt5.QtMultimedia import QSound
 import sys
 import time
@@ -19,9 +19,52 @@ class movingImage(QLabel):
 		self.setAttribute(Qt.WA_NoSystemBackground, True)
 		self.setScaledContents(True)
 
-		self.timer = QTimer(self)
-		self.timer.timeout.connect(self.movement)
-		self.timer.start(10)
+		self.checkProximityTimer = QTimer(self)
+		self.checkProximityTimer.timeout.connect(self.checkProximity)
+		self.checkProximityTimer.start(100)
+
+		self.movementClockwiseTimer = QTimer(self)
+		self.movementClockwiseTimer.timeout.connect(self.movement_clockwise)
+		self.movementClockwiseTimer.start(10)
+
+		self.movementCounterclockwiseTimer = QTimer(self)
+		self.movementCounterclockwiseTimer.timeout.connect(self.movement_counterclockwise)
+
+	def reverseMovementToCounter(self):
+		self.movementClockwiseTimer.stop()
+		self.movementCounterclockwiseTimer.start(10)
+
+	def reverseMovementToClockwise(self):
+		self.movementCounterclockwiseTimer.stop()
+		self.movementClockwiseTimer.start(10)
+
+	def checkProximity(self):
+		pos = QCursor.pos()
+		center_x = self.x() + self.size().width() / 2
+		center_y = self.y() + self.size().height() / 2
+
+		if abs(center_x - pos.x()) < 100 and abs(center_y - pos.y()) < 50:
+			if center_x - pos.x() > 0:
+				if self.movementClockwiseTimer.isActive() and self.is_at_y_bottom_edge():	
+					self.reverseMovementToCounter()
+				elif self.movementCounterclockwiseTimer.isActive() and self.is_at_y_up_edge():
+					self.reverseMovementToClockwise()
+			elif center_x - pos.x() < 0:
+				if self.movementClockwiseTimer.isActive() and self.is_at_y_up_edge():
+					self.reverseMovementToCounter()
+				elif self.movementCounterclockwiseTimer.isActive() and self.is_at_y_bottom_edge():
+					self.reverseMovementToClockwise()
+		if abs(center_x - pos.x()) < 50 and abs(center_y - pos.y()) < 100:
+			if center_y - pos.y() > 0:
+				if self.movementClockwiseTimer.isActive() and self.is_at_x_left_edge():
+					self.reverseMovementToCounter()
+				elif self.movementCounterclockwiseTimer.isActive() and self.is_at_x_right_edge():
+					self.reverseMovementToClockwise()
+			elif center_y - pos.y() < 0:
+				if self.movementClockwiseTimer.isActive() and self.is_at_x_right_edge():
+					self.reverseMovementToCounter()
+				elif self.movementCounterclockwiseTimer.isActive() and self.is_at_x_left_edge():
+					self.reverseMovementToClockwise()
 
 	def move_right(self):
 		self.move(self.x() + 3, self.y())
@@ -55,7 +98,7 @@ class movingImage(QLabel):
 			return True
 		return False
 
-	def movement(self):
+	def movement_clockwise(self):
 		if not self.is_at_x_right_edge() and self.is_at_y_up_edge():
 			self.move_right()
 		elif not self.is_at_y_bottom_edge() and self.is_at_x_right_edge():
@@ -64,6 +107,16 @@ class movingImage(QLabel):
 			self.move_left()
 		elif self.is_at_x_left_edge() and not self.is_at_y_up_edge():
 			self.move_up()
+
+	def movement_counterclockwise(self):
+		if not self.is_at_x_left_edge() and self.is_at_y_up_edge():
+			self.move_left()
+		elif not self.is_at_y_up_edge() and self.is_at_x_right_edge():
+			self.move_up()
+		elif not self.is_at_x_right_edge() and self.is_at_y_bottom_edge():
+			self.move_right()
+		elif self.is_at_x_left_edge() and not self.is_at_y_bottom_edge():
+			self.move_down()
 
 	def mousePressEvent(self, event):
 		if event.button() == Qt.LeftButton:
